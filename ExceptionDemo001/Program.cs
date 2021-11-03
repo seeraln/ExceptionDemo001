33fafd5e224f04b266a7a60cee8166a359a79b10
Seeralan Saththiyaseelan <24449@student.dorset-college.ie>
2021-11-03 17:16:58 +0000

    Ajoutez des fichiers projet.

---
+++ ExceptionDemo001/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ExceptionDemo001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exception Demonstration");
+            Divide(6, 0, "Answer");
+            int[] numbers = new int[2];
+
+            try
+            {
+                numbers[0] = 0;
+                numbers[1] = 1;
+                numbers[2] = 2;
+
+                foreach(int i in numbers)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("An INdexOutOfRangeException was thrown");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("An Generala Exception was thrown");
+            }
+            finally
+            {
+                Console.WriteLine("finished process, exception thrown, have closed all issues");
+            }
+            try
+            {
+                Divide(6, 0, "Answer");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("An DivideByZeroException was thrown");
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+            }
+
+           
+        }
+        private static void Divide(int a, int b, string s)
+        {
+            int result = a / b;
+            Console.WriteLine($"{s.ToUpper()}: {result}");
+        }
+
+        private static void Show(string fname, int age)
+        {
+            if(fname is null)
+            {
+                throw new ArgumentException("Parametre fname cant be null", "fname");
+            }
+            Console.WriteLine($"{fname} is {age} years old");
+        }
+    }
+}
